89c40a38f5ebd10d34d0821666046146d5639905
Stefano Romele <romele.stefano@gmail.com>
2022-02-22 18:12:30 +0100

    callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,28 @@
 import { geocode} from './utils/geocode.js'
 import { forecast } from './utils/forecast.js'
 
-
-forecast(44.1545, -75.7088, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+// add string with city when running node app.js 
+const address = process.argv[2]
+if (!address) {
+  console.log('Please, provide an address')
+} else {
+  geocode(address, (error, data) => {
+    if (error){
+      // stop execution of below code if error is present
+      return console.log(error)
+    } 
+  
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error){
+        return console.log(error)
+      }
+  
+      console.log(data.location)
+      console.log(forecastData)
+    })
   })
+}
+
+
+
+
