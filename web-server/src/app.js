c5c08d9b604c01356864436aeeade6ad848adeb6
Stefano Romele <romele.stefano@gmail.com>
2022-03-05 19:54:11 +0100

    added partials with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 // no need to install path since is a core module
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
+import hbs from 'hbs'
 
 // store express application
 const app = express()
@@ -13,8 +14,16 @@ const __dirname = dirname(__filename)
 const publicDirectory = path.join(__dirname, '../public')
 app.use(express.static(publicDirectory))
 
+// set template views path for name customization
+const viewsPath = path.join(__dirname, '../templates/views')
+
+// set partials template path
+const partialsPath = path.join(__dirname, '../templates/partials')
+
 // set template engine
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // routes
 app.get('', (req, res) => {
@@ -35,7 +44,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Some text for helping user'
+        helpText: 'Some text for helping user',
+        title: 'Help',
+        name: 'Gandalf'
     })
 })
 
