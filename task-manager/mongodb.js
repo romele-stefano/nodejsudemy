0a9a84438a3bd2b03934b9b5d06f9cdbb360ec51
Stefano Romele <romele.stefano@gmail.com>
2022-03-12 18:02:02 +0100

    added new documents into mongoDB collections

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,10 +9,52 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
     }
 
     const db = client.db(databaseName)
-    // add one user
+    /* // add one user
     db.collection('users').insertOne({
         name: 'Aragorn',
         age: 66
+    }, (error, result) => { // callback
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result)
+    }) */
+
+    /* db.collection('users').insertMany([
+        {
+            name: 'Frodo',
+            age: 33
+        }, {
+            name: 'Bilbo',
+            age: 122
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result)
+    }) */
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'task 1',
+            completed: true
+        }, {
+            description: 'task 2',
+            completed: true
+        },
+        {
+            description: 'task 3',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result)
     })
 })
 
