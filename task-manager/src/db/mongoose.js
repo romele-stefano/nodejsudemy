747f7d9dfe91049cf6d71fded055f2febecf9368
Stefano Romele <romele.stefano@gmail.com>
2022-03-13 20:25:26 +0100

    added input validation and sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import validator from 'validator'
 
 const uri = 'mongodb+srv://nox:nox@cluster0.qcfhr.mongodb.net/task-manager?retryWrites=true&w=majority'
 mongoose.connect(uri, {
@@ -8,17 +9,48 @@ mongoose.connect(uri, {
 // define user model
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value){
+            if (value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if (value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 // create new user
-/* const user = new User({
+const user = new User({
     name: 'Stefano',
-    age: 30
+    email: 'test@gmail.com',
+    password: 'testmysecret'
 })
 
 // save user to DB
@@ -26,22 +58,24 @@ user.save().then((result) => {
     console.log(user)
 }).catch((error) => {
     console.log('Error: ', error)
-}) */
+})
 
 // define task model
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 // create new task
 const task = new Task({
-    description: 'Task 1',
-    completed: false
+    description: 'Task 1       '
 })
 
 // save task to DB
