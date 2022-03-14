b3722c3805264d9be83f3661f14e2690b1625923
Stefano Romele <romele.stefano@gmail.com>
2022-03-14 20:45:08 +0100

    added read operations for users and tasks endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,6 +22,28 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).send()
+    })
+})
+
 
 // TASK ENDPOINT
 
@@ -31,6 +53,27 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((err) => {
         res.status(400).send(err)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch((err) => {
+        res.status(500).send()
     })
 })
 
