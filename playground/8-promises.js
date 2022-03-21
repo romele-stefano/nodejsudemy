5b34a8e89f1fe25e98491ccca1c69e41c482b6f7
Stefano Romele <romele.stefano@gmail.com>
2022-03-21 07:25:32 +0100

    added promise chaining in playground

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,4 +1,4 @@
-const doWorkPromise = new Promise((resolve, reject) => {
+/* const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         // resolve([8, 8, 8])
         reject('Cannot solve promise')
@@ -11,4 +11,35 @@ doWorkPromise.then((result) => {
     console.log('Success!', result)
 }).catch((error) => {
     console.log('Error!', error)
+}) */
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// without promise chaining
+/* add(1, 2).then((sum) => {
+    console.log(sum)
+    add(sum, 5).then((sum2) => {
+        console.log(sum2)
+    }).catch((err) => {
+        console.log(err)
+    })
+}).catch((err) => {
+    console.log(err)
+}) */
+
+// PROMISE CHAINING
+add(1, 1).then((sum) => {
+    console.log(sum)
+    // it is important to return the promise
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((err) => {
+    console.log(err)
 })
