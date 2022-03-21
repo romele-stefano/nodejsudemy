f308361ce42e32a20491267295ea86d69a9af25a
Stefano Romele <romele.stefano@gmail.com>
2022-03-21 08:08:19 +0100

    added async-await in task manager playground

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,12 +1,24 @@
 import '../src/db/mongoose.js'
 import { User } from '../src/models/user.js'
 
-User.findByIdAndUpdate('622f934e2d662bc16cd7305e', { age: 31 }).then((user) => {
+/* User.findByIdAndUpdate('622f934e2d662bc16cd7305e', { age: 31 }).then((user) => {
     console.log(user)
     // promise chaining
     return User.countDocuments({ age: 31 })
 }).then((result) => {
     console.log(result)
+}).catch((err) => {
+    console.log(err)
+}) */
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age: age})
+    const count = await User.countDocuments({ age: age })
+    return count
+}
+
+updateAgeAndCount('622f934e2d662bc16cd7305e', 30).then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
 })
--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,11 +1,23 @@
 import '../src/db/mongoose.js'
 import { Task } from '../src/models/task.js'
 
-Task.findByIdAndDelete('622e45148e5c35194b888729').then((task) => {
+/* Task.findByIdAndDelete('622e45148e5c35194b888729').then((task) => {
     console.log(task)
     return Task.countDocuments({ completed: false })
 }).then((result) => {
     console.log(result)
+}).catch((err) => {
+    console.log(err)
+}) */
+
+const deleteTaskAndCount = async (id) =>{
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return task, count
+}
+
+deleteTaskAndCount('622f9517c43d5e42816842fc').then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
 })
