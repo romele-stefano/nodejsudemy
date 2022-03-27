42fc9861b86b9c826242b54df2b15fd556511fd3
Stefano Romele <romele.stefano@gmail.com>
2022-03-27 16:26:45 +0200

    added endpoint for login

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -41,8 +42,28 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// set findByCredentials
+userSchema.statics.findByCredentials = async(email, password) => {
+    const user = await User.findOne({ email: email })
+
+    if (!user){
+        throw new Error('Unable to login')
+    }
+
+    // verify password
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+
 // use middleware
 // standard function because of this binding
+// hash user pwd
 userSchema.pre('save', async function(next) {
     const user = this
     if (user.isModified('password')){
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -14,6 +14,16 @@ userRouter.post('/users', async(req, res) => {
     }
 })
 
+userRouter.post('/users/login', async(req, res) => {
+    try {
+        // findByCredentials defined in user model
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch(err){
+        res.status(400).send()
+    }
+})
+
 userRouter.get('/users', async(req, res) => {
     try {   
         const users = await User.find({})
