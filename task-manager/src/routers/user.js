f0e9c589177d8972d50d00373839831dfd068a37
Stefano Romele <romele.stefano@gmail.com>
2022-04-17 14:24:43 +0200

    added error handling for impage upload endpoint

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -106,6 +106,9 @@ const upload = multer({
 
 userRouter.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+   res.status(400).send({ error: error.message }) 
+   console.log(error.message)
 })
 
 
